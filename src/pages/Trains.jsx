2aa0b33d1df922ba09186935f58b30e670e51a6b
NikDoe <nikd0e@yandex.ru>
2022-09-13 21:02:56 +0300

    feature(tutu): add preloader

--- src/pages/Trains.jsx
+++ src/pages/Trains.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTrains } from '../store/slices/trainSlice';
@@ -7,18 +7,23 @@ import Train from '../components/Train';
 export default function Trains() {
 	const { trains } = useSelector(state => state.trains);
 	const dispatch = useDispatch();
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
+		setIsLoading(true);
 		axios.get('https://tutu-backend.herokuapp.com/api/train').then(response => {
 			dispatch(setTrains(response.data));
+			setIsLoading(false);
 		});
 	}, [dispatch]);
 	return (
 		<div>
 			<h1>Trains page</h1>
-			{trains.map((train, index) => (
-				<Train key={index} {...train} />
-			))}
+			{isLoading ? (
+				<h3>ищем поезда...</h3>
+			) : (
+				trains.map((train, index) => <Train key={index} {...train} />)
+			)}
 		</div>
 	);
 }
