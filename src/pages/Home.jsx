8be861c241be7f7e2af7e048dd473a3c6183017b
NikDoe <nikd0e@yandex.ru>
2022-09-16 14:20:11 +0300

    feature(tutu): disable button
    - disable button when home component render;
    - make the search request to server;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchFrom, setSearchTo } from '../store/slices/trainSlice';
+import { useEffect, useMemo } from 'react';
+import debounce from 'lodash.debounce';
 
 export default function Home() {
 	const { searchFrom, searchTo } = useSelector(state => state.trains);
 	const dispatch = useDispatch();
 
+	const updateSearch = useMemo(() => debounce((str, f) => dispatch(f(str)), 500), [dispatch]);
+
 	const onInputFrom = e => {
-		dispatch(setSearchFrom(e.target.value));
+		updateSearch(e.target.value.trim(), setSearchFrom);
 	};
 
 	const onInputTo = e => {
-		dispatch(setSearchTo(e.target.value));
+		updateSearch(e.target.value.trim(), setSearchTo);
 	};
+
+	useEffect(() => {
+		dispatch(setSearchFrom(''));
+		dispatch(setSearchTo(''));
+	}, [dispatch]);
 
 	return (
 		<div>
--- src/pages/Trains.jsx
+++ src/pages/Trains.jsx
@@ -3,19 +3,23 @@ import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTrains } from '../store/slices/trainSlice';
 import Train from '../components/Train';
+import { Link } from 'react-router-dom';
 
 export default function Trains() {
-	const { trains } = useSelector(state => state.trains);
+	const { trains, searchFrom, searchTo } = useSelector(state => state.trains);
 	const dispatch = useDispatch();
 	const [isLoading, setIsLoading] = useState(true);
 
+	const from = searchFrom ? `searchFrom=${searchFrom}` : '';
+	const to = searchTo ? `searchTo=${searchTo}` : '';
+
 	useEffect(() => {
 		setIsLoading(true);
-		axios.get('https://tutu-backend.herokuapp.com/api/train').then(response => {
+		axios.get(`https://tutu-backend.herokuapp.com/api/train?${from}&${to}`).then(response => {
 			dispatch(setTrains(response.data));
 			setIsLoading(false);
 		});
-	}, [dispatch]);
+	}, [dispatch, from, to]);
 	return (
 		<div>
 			<h1>Trains page</h1>
@@ -24,6 +28,7 @@ export default function Trains() {
 			) : (
 				trains.map((train, index) => <Train key={index} {...train} />)
 			)}
+			<Link to={'/'}>назад к выбору</Link>
 		</div>
 	);
 }
